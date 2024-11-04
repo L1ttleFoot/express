71382b0533c11e3dc1cb37c258f5c6d009d24c0a
Littl <leonid.klinck@yandex.ru>
2024-11-04 13:58:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express()
+const PORT = 8080
+
+app.get('/api', (req, res) => {
+    res.send('Hello, World!');
+});
+
+async function startApp() {
+    try{
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+        
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+startApp()
