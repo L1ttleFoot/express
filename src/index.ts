e5606d09b4f880c744bee37668247278fc6839ac
Littl <leonid.klinck@yandex.ru>
2025-02-04 00:07:04 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
 app.use(router);
 
 app.listen(PORT, () => {
